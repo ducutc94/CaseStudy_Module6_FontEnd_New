399819340e25ab3584fca21da22124e413587d29
thainguyen68 <nvt.689@gmail.com>
2023-08-08 21:01:26 +0700

    display icon see passwork

--- src/user/formlogin/Login.js
+++ src/user/formlogin/Login.js
@@ -109,8 +109,6 @@ export default function Login({showLogin, handleClose}) {
                                                       <FontAwesomeIcon icon={faEyeSlash} />
                                                     </span>
                                                 )}
-
-
                                             </div>
                                         </div>
 
